b0ba8df5a8cd3cd3c019168c42b934a79915b728
unknown <elonmusk0346@gmail.com>
2025-07-24 15:38:27 +0500

    first commit

--- front-end/src/features/characters/charactersSlice.js
+++ front-end/src/features/characters/charactersSlice.js
@@ -1,2 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const charactersApi = createApi({
+    reducerPath: 'charactersApi',
+    baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:8000/api/` }),
+    endpoints: (builder) => {
+        getCharacters: builder.query({
+            query: () => `/characters/?saga=${saga}`
+        })
+    }
+})
